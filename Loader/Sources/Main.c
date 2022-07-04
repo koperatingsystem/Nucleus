5f3641e0ed80595824410fe60d92971d7e1043bd
ProtoByter <bobrow2006@gmail.com>
2022-07-04 21:35:17 +0100

    Added various features: - ISO Building - FB Initialization Currently can't draw to the FB due to a lack of a GDT

--- Loader/Sources/Main.c
+++ Loader/Sources/Main.c
@@ -1,4 +1,8 @@
+#include <defs.h>
+#include <multiboot.h>
 
 void lmain(const void* mb_struct) {
+    // TODO: Draw to framebuffer
 
+    _exit();
 }
---
+++ Shared/iso-tools.cmake
@@ -0,0 +1,25 @@
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/staging)
+
+find_program(XORRISO_PATH xorriso REQUIRED)
+find_program(GRUBMKRESCUE_PATH grub-mkrescue REQUIRED)
+
+add_custom_target(
+        build-iso
+        ${GRUBMKRESCUE_PATH} -o boot.iso -v ${CMAKE_BINARY_DIR}/staging
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        COMMENT "Building a boot ISO"
+)
+
+function(add_file_iso file target)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/staging/${target})
+    file(COPY ${file} DESTINATION ${CMAKE_BINARY_DIR}/staging/${target})
+endfunction()
+
+function(add_target_output_iso target target_dir)
+    add_custom_command(
+            TARGET ${target} POST_BUILD VERBATIM
+            COMMAND ${CMAKE_COMMAND} -E copy "$<TARGET_FILE:${target}>" ${CMAKE_BINARY_DIR}/staging/${target_dir}
+    )
+
+    add_dependencies(build-iso ${target})
+endfunction()
